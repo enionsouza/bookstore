19e497cb20696ca612037406c10984c0517b19d3
Enio N. de Souza <enionsouza@gmail.com>
2021-09-28 13:04:00 -0400

    chore: Make required changes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/configureStore';
 import NavBar from './components/NavBar';
-import App from './App';
+import Books from './pages/books';
+import Categories from './pages/categories';
+import './components/index.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router basename="/">
       <NavBar />
-      <App />
+      <Provider store={store}>
+        <Switch>
+          <Route exact path="/">
+            <Books />
+          </Route>
+          <Route exact path="/categories">
+            <Categories />
+          </Route>
+        </Switch>
+      </Provider>
     </Router>
   </React.StrictMode>,
   document.getElementById('root'),
