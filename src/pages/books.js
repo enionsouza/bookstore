8f1417fc87a02f1d832ac1bdeaa1b2785d54d112
Enio N. de Souza <enionsouza@gmail.com>
2021-09-28 11:31:39 -0400

    feat: Add 'BookItem' component

--- src/pages/books.js
+++ src/pages/books.js
@@ -2,15 +2,15 @@ import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import {
-  loadBooks, createBook, removeBook,
+  loadBooks, createBook,
 } from '../redux/books/books';
+import BookItem from '../components/BookItem';
 
 const Books = () => {
   const books = useSelector((state) => state.books);
   const dispatch = useDispatch();
   const loadBooksAction = bindActionCreators(loadBooks, dispatch);
   const createBookAction = bindActionCreators(createBook, dispatch);
-  const removeBookAction = bindActionCreators(removeBook, dispatch);
 
   useEffect(() => {
     loadBooksAction();
@@ -27,21 +27,24 @@ const Books = () => {
       <ul>
         {books.map((book) => (
           <li key={book.id}>
-            <span>
-              {book.title}
-              {' '}
-              by
-              {' '}
-              {book.author}
-            </span>
-            <button type="button" onClick={() => removeBookAction(book.id)}>Remove</button>
+            <BookItem title={book.title} author={book.author} bookId={book.id} />
           </li>
         ))}
       </ul>
       <form>
         <input type="text" name="bookTitle" placeholder="Add a title..." />
         <input type="text" name="bookAuthor" placeholder="Add an author name..." />
-        <button type="submit" onClick={() => createBookAction(newBook)}>Submit</button>
+        <button
+          type="submit"
+          onClick={
+            (e) => {
+              e.preventDefault();
+              createBookAction(newBook);
+            }
+          }
+        >
+          Submit
+        </button>
       </form>
     </>
   );
