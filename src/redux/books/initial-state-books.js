e1c303c2c5908c87a615aebb4b2ac1465b84381b
Enio N. de Souza <enionsouza@gmail.com>
2021-09-28 21:01:18 -0400

    fix: Make all hardcoded ids as strings

--- src/redux/books/initial-state-books.js
+++ src/redux/books/initial-state-books.js
@@ -2,52 +2,52 @@ const initialStateBooks = [
   {
     author: 'Chinua Achebe',
     title: 'Things Fall Apart',
-    id: 1958,
+    id: '1958',
   },
   {
     author: 'Hans Christian Andersen',
     title: 'Fairy tales',
-    id: 1836,
+    id: '1836',
   },
   {
     author: 'Dante Alighieri',
     title: 'The Divine Comedy',
-    id: 1315,
+    id: '1315',
   },
   {
     author: 'Unknown',
     title: 'The Epic Of Gilgamesh',
-    id: -1700,
+    id: '-1700',
   },
   {
     author: 'Unknown',
     title: 'The Book Of Job',
-    id: -600,
+    id: '-600',
   },
   {
     author: 'Unknown',
     title: 'One Thousand and One Nights',
-    id: 1200,
+    id: '1200',
   },
   {
     author: 'Unknown',
     title: "Nj\u00e1l's Saga",
-    id: 1350,
+    id: '1350',
   },
   {
     author: 'Jane Austen',
     title: 'Pride and Prejudice',
-    id: 1813,
+    id: '1813',
   },
   {
     author: 'Honor\u00e9 de Balzac',
     title: 'Le P\u00e8re Goriot',
-    id: 1835,
+    id: '1835',
   },
   {
     author: 'Samuel Beckett',
     title: 'Molloy, Malone Dies, The Unnamable, the trilogy',
-    id: 1952,
+    id: '1952',
   },
 ];
 
