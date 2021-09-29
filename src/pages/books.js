28ff363a5a2eb9bfe96af6c97ef79438928a7fb1
Enio N. de Souza <enionsouza@gmail.com>
2021-09-28 21:08:54 -0400

    feat: Add FormAddBook component

--- src/pages/books.js
+++ src/pages/books.js
@@ -1,26 +1,19 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import {
-  loadBooks, createBook,
-} from '../redux/books/books';
+import { loadBooks } from '../redux/books/books';
 import BookItem from '../components/BookItem';
+import FormAddBook from '../components/FormAddBook';
 
 const Books = () => {
   const books = useSelector((state) => state.books);
   const dispatch = useDispatch();
   const loadBooksAction = bindActionCreators(loadBooks, dispatch);
-  const createBookAction = bindActionCreators(createBook, dispatch);
 
   useEffect(() => {
     loadBooksAction();
     return null;
   }, []);
-
-  const newBook = {
-    author: 'Giovanni Boccaccio',
-    title: 'The Decameron',
-  };
 
   return (
     <>
@@ -31,21 +24,7 @@ const Books = () => {
           </li>
         ))}
       </ul>
-      <form>
-        <input type="text" name="bookTitle" placeholder="Add a title..." />
-        <input type="text" name="bookAuthor" placeholder="Add an author name..." />
-        <button
-          type="submit"
-          onClick={
-            (e) => {
-              e.preventDefault();
-              createBookAction(newBook);
-            }
-          }
-        >
-          Submit
-        </button>
-      </form>
+      <FormAddBook />
     </>
   );
 };
