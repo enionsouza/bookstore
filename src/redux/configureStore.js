faef721acc4cadef7f54a6e2c248fdc8002ec181
Enio N. de Souza <enionsouza@gmail.com>
2021-09-29 19:45:53 -0400

    chore: Add 'thunk' middleware

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,12 +1,13 @@
 import { combineReducers, createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import bookReducer from './books/books';
 
-const reducers = combineReducers({ books: bookReducer });
+const rootReducer = combineReducers({ books: bookReducer });
 
 const store = createStore(
-  reducers,
-  applyMiddleware(logger),
+  rootReducer,
+  applyMiddleware(logger, thunk),
 );
 
 export default store;
